d0d43879da96d6549f3556f9482aab3fc9a39538
Jason Rodriguez <jason@frenteventures.com>
2018-06-09 13:19:56 -0600

    Added cart changes

--- functions.php
+++ functions.php
@@ -19,17 +19,18 @@ include_once STYLESHEETPATH . '/includes/shortcodes.php';
 include_once STYLESHEETPATH . '/includes/learndash.php';
 include_once STYLESHEETPATH . '/includes/woocommerce.php';
 
-add_action( 'wp_enqueue_scripts', 'astra_parent_theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'astra_parent_theme_enqueue_styles');
 
-function astra_parent_theme_enqueue_styles() {
-	wp_enqueue_style( 'astra-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'astrai-child-style',
-		get_stylesheet_directory_uri() . '/style.css',
-		array( 'astra-style' )
-	);
-  wp_enqueue_style( 'font-awesome',
-    'https://use.fontawesome.com/releases/v5.0.13/css/all.css'
-  );
+function astra_parent_theme_enqueue_styles()
+{
+    wp_enqueue_style('astra-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('astrai-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('astra-style')
+    );
+    wp_enqueue_style('font-awesome',
+        'https://use.fontawesome.com/releases/v5.0.13/css/all.css'
+    );
 }
 
 /**
@@ -39,11 +40,12 @@ function astra_parent_theme_enqueue_styles() {
  * @param array $vars - an empty array to push permitted query parameters onto
  * @return void
  */
-function add_query_vars_filter( $vars ) {
-	$vars[] = 'course';
-	return $vars;
+function add_query_vars_filter($vars)
+{
+    $vars[] = 'course';
+    return $vars;
 }
-add_filter( 'query_vars', 'add_query_vars_filter' );
+add_filter('query_vars', 'add_query_vars_filter');
 
 /**
  * Redirect away from registration page if the user is logged in. If
@@ -52,55 +54,37 @@ add_filter( 'query_vars', 'add_query_vars_filter' );
  *
  * @return void
  */
-function ld_redirect_actions() {
-	global $wp;
+function ld_redirect_actions()
+{
+    global $wp;
 
-	$course = get_query_var( 'course' );
-	$current_url = home_url( $wp->request );
+    $course = get_query_var('course');
+    $current_url = home_url($wp->request);
 
-	$is_logged_in = is_user_logged_in();
-  $is_account_page = is_account_page( $current_url );
-	$is_register_page = is_register_page( $current_url );
+    $is_logged_in = is_user_logged_in();
+    $is_account_page = is_account_page($current_url);
+    $is_register_page = is_register_page($current_url);
 
-  if ( $is_logged_in ) {
-    if ( $is_register_page && $course ) {
-      wp_redirect( ld_get_course_enrollment_page( $course ) );
-      exit();
+    if ($is_logged_in) {
+        if ($is_register_page && $course) {
+            wp_redirect(ld_get_course_enrollment_page($course));
+            exit();
+        } else if ($is_register_page) {
+            wp_redirect(get_site_url());
+            exit();
+        } else if (is_front_page() && user_can(get_current_user_id(), 'group_leader')) {
+            wp_redirect(get_site_url() . '/overview');
+            exit();
+        } else if (is_front_page()) {
+            wp_redirect(get_site_url() . '/profile');
+            exit();
+        }
     }
-    else if ( $is_register_page ) {
-      wp_redirect( get_site_url() );
-      exit();
-    }
-    else if ( is_front_page() && user_can( get_current_user_id(), 'group_leader' )) {
-      wp_redirect( get_site_url() . '/overview');
-      exit();
-    }
-    else if ( is_front_page() ) {
-      wp_redirect( get_site_url() . '/profile' );
-      exit();
-    }
-  }
 }
-add_action( 'wp', 'ld_redirect_actions' );
+add_action('wp', 'ld_redirect_actions');
 
-<<<<<<< HEAD
-function logged_in_header() {
-	if ( is_user_logged_in() ) { ?>
-    <div class="logged-in-header">
-    <h5>
-      <b>
-        <?php echo wp_get_current_user()->user_login; ?>
-      </b>
-    </h5>
-    </div>
-  <?php
-	}
-}
-add_action( 'astra_header_before', 'logged_in_header' );
- 
-=======
 /* function logged_in_header() { */
-/* 	if ( is_user_logged_in() ) { ?> */
+/*     if ( is_user_logged_in() ) { ?> */
 /*     <div class="logged-in-header"> */
 /*     <h5> */
 /*       <b> */
@@ -109,7 +93,6 @@ add_action( 'astra_header_before', 'logged_in_header' );
 /*     </h5> */
 /*     </div> */
 /*   <?php */
-/* 	} */
+/*     } */
 /* } */
 /* add_action( 'astra_header_before', 'logged_in_header' ); */
->>>>>>> master
--- includes/woocommerce.php
+++ includes/woocommerce.php
@@ -1,18 +1,44 @@
+
 <?php
-add_filter( 'woocommerce_product_add_to_cart_text', 'custom_add_to_cart_text' );
-function custom_add_to_cart_text() {
-  return __( 'Enroll', 'nationwide-educators' );
+// This changes the add to cart button to enroll
+add_filter('woocommerce_product_add_to_cart_text', 'custom_add_to_cart_text');
+function custom_add_to_cart_text()
+{
+    return __('Enroll', 'nationwide-educators');
 }
 
-add_filter( 'woocommerce_pay_order_button_text', 'custom_order_button_text' );
-function custom_order_button_text() {
-  return __ ( 'SOME ORDER BUTTON', 'nationwide-educators' );
+add_filter('woocommerce_pay_order_button_text', 'custom_order_button_text');
+function custom_order_button_text()
+{
+    return __('SOME ORDER BUTTON', 'nationwide-educators');
 }
 
-function woocommerce_button_proceed_to_checkout() {
-  $checkout_url = WC()->cart->get_checkout_url(); ?>
-  <a href="<?php echo esc_url( wc_get_checkout_url() );?>" class="checkout-button button alt wc-forward">
-  <?php esc_html_e( 'Purchase Courses', 'woocommerce' ); ?>
+function woocommerce_button_proceed_to_checkout()
+{
+    $checkout_url = WC()->cart->get_checkout_url();?>
+  <a href="<?php echo esc_url(wc_get_checkout_url()); ?>" class="checkout-button button alt wc-forward">
+  <?php esc_html_e('Purchase Courses', 'woocommerce');?>
   </a>
   <?php
- }
+}
+
+/**
+ * This removes the ability to register in the same course multiple times along with purchasing amounts
+ */
+function wc_remove_all_quantity_fields($return, $product)
+{
+    return true;
+}
+add_filter('woocommerce_is_sold_individually', 'wc_remove_all_quantity_fields', 10, 2);
+
+// This changes the Continue Shopping Text
+add_filter('wc_add_to_cart_message_html', 'my_changed_wc_add_to_cart_message_html', 10, 2);
+function my_changed_wc_add_to_cart_message_html($message, $products)
+{
+    if (strpos($message, 'Continue shopping') !== false) {
+        $message = str_replace("Continue shopping", "Enroll in other courses", $message);
+    }
+    return $message;
+
+}
+?>
